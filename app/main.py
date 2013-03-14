391a7a1b1b875a86262b62f4feee149980e93b23
Kauhsa <kauhsa@viuhka.fi>
2013-03-14 18:05:47 +0200

    use PORT environment variable, for heroku

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, request, session, redirect
 from models import db, bcrypt, User
 
@@ -50,4 +51,5 @@ def logged_user():
         return jsonify(logged_user=session['logged_user_id'])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
